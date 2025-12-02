5483870336ab5ab9c3ef236ed1fff4d8a59fec9c
tebibu21 <tebibu211@gmail.com>
2025-12-02 20:04:24 +0300

    new question solved

---
+++ 0081 Search in Rotated Sorted Array II/81. Search in Rotated Sorted Array II.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return True
+
+            # If duplicates are at the edges, shrink the window
+            if nums[left] == nums[mid] == nums[right]:
+                left += 1
+                right -= 1
+            # Left half is sorted
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # Right half is sorted
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return False
